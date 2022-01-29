a58eb8405b43ea179e5ed744b23669f087de1bd2
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2022-01-29 21:10:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ setting_title = setting_title + ' ' + setting_version
 setting_font = 'Arial'
 # Font Size
 setting_font_size = 14
-# Window Size
+# graphical_window Size
 setting_window_size = [650, 250]
 
 def interface():
@@ -26,13 +26,13 @@ def interface():
     address_destination = address_default + '/Desktop/Factorio/'
 
     graphical_layout.append([gui.Text('Mod List Address:', font=(setting_font, setting_font_size))])
-    graphical_layout.append([gui.Input(address_mod_source, key='addr_mod', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_mod', initial_folder=address_mod_source, font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Input(address_mod_source, key='address_mod_source_folder', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='address_mod_source_folder', initial_folder=address_mod_source, font=(setting_font, setting_font_size))])
     graphical_layout.append([gui.Text('Target Address:', font=(setting_font, setting_font_size))])
-    graphical_layout.append([gui.Input(address_destination, key='addr_copy', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_copy', initial_folder=address_destination, font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Input(address_destination, key='address_destination_folder', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='address_destination_folder', initial_folder=address_destination, font=(setting_font, setting_font_size))])
     graphical_layout.append([gui.Button('Start Copy', font=(setting_font, setting_font_size))])    
 
-    def copy_mod(mod_list_addr, mod_copy_addr, window):
-        with open(mod_list_addr + 'mod-list.json') as json_file:
+    def copy_mod(address_mod_list, address_mod_copy, graphical_window):
+        with open(address_mod_list + 'mod-list.json') as json_file:
             mod_list_json = json.load(json_file)
             json_file.close()
 
@@ -49,7 +49,7 @@ def interface():
             gui.popup_ok('Mods are not selected in game.', title=setting_title, font=(setting_font, setting_font_size))
             return
 
-        mod_file_list = next(os.walk(mod_list_addr), (None, None, []))[2]
+        mod_file_list = next(os.walk(address_mod_list), (None, None, []))[2]
         mod_file_list_compare = []
 
         for i in range(len(mod_file_list)):
@@ -60,43 +60,43 @@ def interface():
                 if re.search(str('^.*(' + str(mod_list[i]) + ').*$'), str(mod_file_list_compare[j])):
                     mod_res.append([mod_list[i], mod_file_list[j]])
 
-        if not os.path.exists(mod_copy_addr):
-            os.mkdir(mod_copy_addr, 0o666)
+        if not os.path.exists(address_mod_copy):
+            os.mkdir(address_mod_copy, 0o666)
 
         graphical_layout = [[]]
         graphical_layout.append([gui.Text('Progress:', font=(setting_font, setting_font_size))])
         graphical_layout.append([gui.Text('', key='info', font=(setting_font, setting_font_size))])
         graphical_layout.append([gui.ProgressBar(len(mod_res), orientation='h', size=(100, 20), key='progress_bar')])
         graphical_layout.append([gui.Cancel(font=(setting_font, setting_font_size))])
-        window.hide()
-        window2 = gui.Window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
+        graphical_window.hide()
+        graphical_window2 = gui.graphical_window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
 
         for i in range(len(mod_res)):
-            window2['info'].update(str('Copying (' + str(i + 1) + '/' + str(len(mod_res)) + ') ' + str(mod_res[i][0])))
-            window2['progress_bar'].UpdateBar(i + 1)
-            shutil.copyfile(str(mod_list_addr) + str(mod_res[i][1]), str(mod_copy_addr) + str(mod_res[i][1]))
+            graphical_window2['info'].update(str('Copying (' + str(i + 1) + '/' + str(len(mod_res)) + ') ' + str(mod_res[i][0])))
+            graphical_window2['progress_bar'].UpdateBar(i + 1)
+            shutil.copyfile(str(address_mod_list) + str(mod_res[i][1]), str(address_mod_copy) + str(mod_res[i][1]))
             
-            event, values = window2.Read(timeout=10)
+            event, values = graphical_window2.Read(timeout=10)
 
             if event in (None, 'Exit', 'Cancel', gui.WIN_CLOSED): 
-                window2.close()
+                graphical_window2.close()
                 return
 
         gui.popup_ok('Tasks have completed successfully.', title=setting_title, font=(setting_font, setting_font_size))
-        window2.close()
+        graphical_window2.close()
 
-    window = gui.Window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
+    graphical_window = gui.graphical_window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
 
     while True:
-        event, values = window.Read(timeout=100)
+        event, values = graphical_window.Read(timeout=100)
 
         if event in (None, 'Exit', 'Cancel', gui.WIN_CLOSED): 
-            window.close()
+            graphical_window.close()
             break
 
         elif event in ('Start Copy'):
-            copy_mod(str(values['addr_mod']) + '/', str(values['addr_copy'])+ '/', window)
-            window.close()
+            copy_mod(str(values['address_mod_source_folder']) + '/', str(values['address_destination_folder'])+ '/', graphical_window)
+            graphical_window.close()
             break
 
 interface()
