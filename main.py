e7a734ad69e39e9e4fc75d8a17202283e494fe02
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2022-01-29 21:06:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import shutil
 import PySimpleGUI as gui
 
 # Edition, Version, Revision
-setting_edition = 3
-setting_revision = 6
+setting_edition = 4
+setting_revision = 1
 setting_version = str(setting_edition) + '.' + str(setting_revision)
 # Title
 setting_title = 'EXP MOD COPY'
@@ -19,17 +19,17 @@ setting_font_size = 14
 setting_window_size = [650, 250]
 
 def interface():
-    layout = [[]]
-    default_addr = os.path.expanduser('~')
-    default_addr = default_addr.replace('\\', '/')
-    addr_mod_def = default_addr + '/AppData/Roaming/Factorio/mods/'
-    addr_copy_def = default_addr + '/Desktop/Factorio/'
+    graphical_layout = [[]]
+    address_default = os.path.expanduser('~')
+    address_default = address_default.replace('\\', '/')
+    address_mod_source = address_default + '/AppData/Roaming/Factorio/mods/'
+    address_destination = address_default + '/Desktop/Factorio/'
 
-    layout.append([gui.Text('Mod List Address:', font=(setting_font, setting_font_size))])
-    layout.append([gui.Input(addr_mod_def, key='addr_mod', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_mod', initial_folder=addr_mod_def, font=(setting_font, setting_font_size))])
-    layout.append([gui.Text('Target Address:', font=(setting_font, setting_font_size))])
-    layout.append([gui.Input(addr_copy_def, key='addr_copy', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_copy', initial_folder=addr_copy_def, font=(setting_font, setting_font_size))])
-    layout.append([gui.Button('Start Copy', font=(setting_font, setting_font_size))])    
+    graphical_layout.append([gui.Text('Mod List Address:', font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Input(address_mod_source, key='addr_mod', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_mod', initial_folder=address_mod_source, font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Text('Target Address:', font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Input(address_destination, key='addr_copy', font=(setting_font, setting_font_size)), gui.FolderBrowse(target='addr_copy', initial_folder=address_destination, font=(setting_font, setting_font_size))])
+    graphical_layout.append([gui.Button('Start Copy', font=(setting_font, setting_font_size))])    
 
     def copy_mod(mod_list_addr, mod_copy_addr, window):
         with open(mod_list_addr + 'mod-list.json') as json_file:
@@ -63,13 +63,13 @@ def interface():
         if not os.path.exists(mod_copy_addr):
             os.mkdir(mod_copy_addr, 0o666)
 
-        layout = [[]]
-        layout.append([gui.Text('Progress:', font=(setting_font, setting_font_size))])
-        layout.append([gui.Text('', key='info', font=(setting_font, setting_font_size))])
-        layout.append([gui.ProgressBar(len(mod_res), orientation='h', size=(100, 20), key='progress_bar')])
-        layout.append([gui.Cancel(font=(setting_font, setting_font_size))])
+        graphical_layout = [[]]
+        graphical_layout.append([gui.Text('Progress:', font=(setting_font, setting_font_size))])
+        graphical_layout.append([gui.Text('', key='info', font=(setting_font, setting_font_size))])
+        graphical_layout.append([gui.ProgressBar(len(mod_res), orientation='h', size=(100, 20), key='progress_bar')])
+        graphical_layout.append([gui.Cancel(font=(setting_font, setting_font_size))])
         window.hide()
-        window2 = gui.Window(setting_title, layout=layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
+        window2 = gui.Window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
 
         for i in range(len(mod_res)):
             window2['info'].update(str('Copying (' + str(i + 1) + '/' + str(len(mod_res)) + ') ' + str(mod_res[i][0])))
@@ -85,7 +85,7 @@ def interface():
         gui.popup_ok('Tasks have completed successfully.', title=setting_title, font=(setting_font, setting_font_size))
         window2.close()
 
-    window = gui.Window(setting_title, layout=layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
+    window = gui.Window(setting_title, graphical_layout=graphical_layout, size=(setting_window_size[0], setting_window_size[1]), resizable=False, finalize=True)
 
     while True:
         event, values = window.Read(timeout=100)
